c5ddc22a54b51d64f56c4f2b4147559776529c6f
Rafael de Oliveira Carvalho <rafael.carvalho@ad.ufrgs.br>
2018-11-22 18:15:49 -0200

    2018/11/22

---
+++ ReceitasUnity/Assets/_Scripts/CustomCursorPointer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursorPointer : MonoBehaviour {
+	public Texture2D cursorTexture2D;
+	private CursorMode cursorMode = CursorMode.Auto;
+	private Vector2 hotSpot = Vector2.zero;
+	public void OnMouseEnter () {
+		SetCustomCursor (cursorTexture2D);
+	}
+	public void OnMouseExit () {
+		SetCustomCursor (null);
+	}
+
+	private void SetCustomCursor (Texture2D curText) {
+		Cursor.SetCursor (curText, hotSpot, cursorMode);
+	}
+}
